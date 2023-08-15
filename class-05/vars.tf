061eef9b51e09d72104ef59dd0fdd7583392a33d
Cleison Melo <cleison.ferreirademelo@experian.com>
2023-08-15 12:44:55 -0500

    class-05

---
+++ class-05/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_ACCESS_KEY" {
+
+}
+variable "AWS_SECRET_KEY" {
+
+}
+variable "AWS_REGION" {
+  default = "eu-central-1"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    eu-central-1 = "ami-123456"
+    eu-east-1    = "ami-456789"
+    us-west-1    = "ami-963258"
+  }
+}
